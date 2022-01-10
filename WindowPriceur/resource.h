e2c2eafc8392a6e13342512ec9b7dbb8ecfed334
AlexandreHdz <70096973+AlexandreHdz@users.noreply.github.com>
2022-01-10 10:03:36 +0100

    Add files via upload

---
+++ WindowPriceur/resource.h
@@ -0,0 +1,67 @@
+
+// Source.cpp ----------------------------------------------------------------------------------
+#define IDC_STATIC						-1
+
+
+#define IDD_PRICE						1001
+#define IDD_RESULT						1002
+#define IDD_RESULTBAR					1003
+#define IDD_PRICEBAR					1004
+#define IDD_PB							1080
+
+// BOUTON MAIN SCREEN
+#define IDB_CALLEU						1005
+#define IDB_PUTEU						1006
+#define IDB_CALLAS						1007
+#define IDB_PUTAS						1008
+#define IDB_CALLKO						1009
+#define IDB_PUTKO						1010
+#define IDB_CALLRKO						1011
+#define IDB_PUTRKO						1012
+
+#define IDC_SPOT						1013
+#define IDC_STRIKE						1014
+#define IDC_MAT							1015
+#define IDC_TAUX						1016
+#define IDC_VOL							1017
+#define IDC_PRICER						1018
+#define IDC_BACK						1019
+#define IDC_BAR							1020
+
+#define IDC_SPOT2						1021
+#define IDC_STRIKE2						1022
+#define IDC_MAT2						1023
+#define IDC_TAUX2						1024
+#define IDC_VOL2						1025
+#define IDC_BAR2						1026
+
+
+#define IDC_RESULT						1027
+#define IDC_OK							1028
+#define IDC_RESULT2						1029
+
+#define IDC_DELTA						1030
+#define IDC_GAMMA						1031
+#define IDC_VEGA						1032
+#define IDC_THETA						1033
+#define IDC_RHO							1034
+
+#define ID_BS							1035
+#define ID_MC							1036
+#define IDE_PB							1037
+#define ID_GO							1078
+#define IDC_QUIT						1079
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+#pragma once
+
+
