f1744579629a2f6582a43169b4328f4c69e01b43
AlexandreHdz <70096973+AlexandreHdz@users.noreply.github.com>
2022-01-27 17:37:04 +0100

    Add files via upload

---
+++ WindowPriceur_27012022/WindowPriceur/WindowPriceur/resource.h
@@ -0,0 +1,91 @@
+
+// Source.cpp ----------------------------------------------------------------------------------
+#define IDC_STATIC						-1
+
+
+#define IDD_PRICE						1001
+#define IDD_RESULT						1002
+#define IDD_RESULTBAR					1003
+#define IDD_PRICEBAR					1004
+#define IDD_PB							1080
+
+// BOUTON MAIN SCREEN
+#define IDB_CALLEU						1005
+#define IDB_PUTEU						1006
+#define IDB_CALLAS						1007
+#define IDB_PUTAS						1008
+#define IDB_CALLKO						1009
+#define IDB_PUTKO						1010
+#define IDB_CALLRKO						1011
+#define IDB_PUTRKO						1012
+
+#define IDC_SPOT						1013
+#define IDC_STRIKE						1014
+#define IDC_MAT							1015
+#define IDC_TAUX						1016
+#define IDC_VOL							1017
+#define IDC_PRICER						1018
+#define IDC_BACK						1019
+#define IDC_BAR							1020
+
+#define IDC_SPOT2						1021
+#define IDC_STRIKE2						1022
+#define IDC_MAT2						1023
+#define IDC_TAUX2						1024
+#define IDC_VOL2						1025
+#define IDC_BAR2						1026
+
+
+#define IDC_RESULT						1027
+#define IDC_OK							1028
+#define IDC_RESULT2						1029
+
+#define IDC_DELTA						1030
+#define IDC_GAMMA						1031
+#define IDC_VEGA						1032
+#define IDC_THETA						1033
+#define IDC_RHO							1034
+
+#define ID_BS							1035
+#define ID_MC							1036
+#define IDE_PB							1037
+#define ID_GO							1078
+#define IDC_QUIT						1079
+
+#define IDD_YAHOO						1090
+#define ID_DONNEE						1086
+#define IDC_DD							1081
+#define IDC_DF							1082
+#define IDC_TICKER						1083
+#define IDC_RET							1084
+#define IDC_VALID						1085
+
+#define IDD_OUTPUT						1900
+
+
+#define IDS_DATE						1
+#define IDS_OPEN				        2
+#define IDS_HIGH				        3
+#define IDS_LOW					        4
+#define IDS_CLOS				        5
+#define IDS_CLOSADJ						6
+#define IDS_VOLUME						7
+
+
+#define IDC_VARIANCE					1990
+#define IDC_REND						1999
+#define IDC_STATICVAR					1991
+#define IDC_STATICREND					1998
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+#pragma once
+
+
---
+++ WindowPriceur_27012022/WindowPriceur/WindowPriceur/utilitaire.h
@@ -0,0 +1,314 @@
+#pragma once
+
+
+#ifndef utilitaire_h
+#define utilitaire_h
+
+#define M_PI 3.14159265358979323846
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <random>
+#include "Matrice.h"
+#define nb_pas 252.0
+
+#include <sstream>
+
+
+
+
+std::vector<double> vecteur_pas(double T) {
+    std::vector<double> pas;
+    pas.push_back(0);
+    double x = T / nb_pas;
+    for (int i = 1; i <= nb_pas; i++) {
+        pas.push_back(pas[i - 1] + x);
+    }
+    return pas;
+}
+
+//Calcul la somme des elements d'un vecteur
+double somme_trajLog(std::vector <double> traj) {
+    double somme = 0;
+    for (int i = 0; i <= traj.size() - 1; i++) {
+        somme = somme + log(traj[i]);
+    }
+    return somme;
+}
+
+double somme_traj(std::vector <double> traj) {
+    double somme = 0;
+    for (int i = 0; i <= traj.size() - 1; i++) {
+        somme = somme + traj[i];
+    }
+    return somme;
+}
+
+// Renvoie le plus grand elements d'un vecteur
+double valeur_max(std::vector <double> traj) {
+    double stockage = traj[0];
+    for (int i = 1; i <= traj.size() - 1; i++) {
+        if (traj[i] > traj[i - 1]) {
+            stockage = traj[i];
+        }
+        else {
+            stockage = stockage;
+        }
+    }
+    return stockage;
+}
+
+
+//vecteur de 252 valeur alea N(0,1)
+std::vector <double> NormalRnd() {
+    std::vector <double> brow;
+    std::random_device gen;
+    std::normal_distribution<> d{ 0,1 };
+
+    for (int i = 0; i < nb_pas; i++) {
+        brow.push_back(d(gen));
+    }
+    return brow;
+}
+// Simule la trajectoire d'un mouvement brownien
+std::vector <double> simu_brownien(double T) {
+    std::vector <double> brow;
+    brow.push_back(0);
+    std::random_device gen;
+    std::normal_distribution<> d{ 0,1 };    
+    double pas = T / nb_pas;
+    for (int i = 0; i < nb_pas; i++) {
+        brow.push_back(brow[i] + sqrt(pas)*d(gen));
+    }
+    return brow;
+}
+
+//Pour pricer les options européennes
+//std::vector<double> MBG(double S, double mu, double vol, double T) {
+//    std::vector<double> traj;
+//    traj.push_back(S);
+//    std::vector <double> b = simu_brownien(T);
+//    for (int i = 1; i < T*252; i++) {
+//        traj.push_back(S * exp((mu - (pow(vol, 2) / 2)) * (i /252) + vol * b[i]));
+//    }
+//    return traj;
+//}
+
+//Trajectoire de prix avec GBM
+std::vector<double> MBG(double S, double mu, double vol, double T) {
+    std::vector<double> traj;
+    traj.push_back(S);
+
+    std::random_device gen;
+    std::normal_distribution<> d{ 0,1 };
+
+    double pas = T / nb_pas;
+    double drift = 0.0;
+    double diffusion = 0.0;
+
+    for (int i = 1; i < nb_pas; i++) {
+        double b = d(gen);
+        drift = (mu - (double)0.5 * pow(vol, (int)2)) * pas;
+        diffusion = vol * b * sqrt(pas);
+        traj.push_back(traj[i - 1] * exp(drift + diffusion));
+    }
+    return traj;
+}
+
+
+
+//Pour pricer les options Asiatique
+std::vector<double> Euler(double S, double mu, double vol, double T) {
+    std::vector <double> traj;
+    std::vector <double> b = simu_brownien(T);
+    traj.push_back(S);
+    double pas = T / nb_pas;
+    for (int i = 1; i < nb_pas; i++) {
+        traj.push_back(traj[i - 1] +pas * mu * traj[i - 1] + traj[i - 1] * vol * (b[i] - b[i - 1]));
+    }
+    return traj;
+}
+
+
+
+
+//Loi normal standard
+double norm_pdf(const double x) {
+    return (1.0 / (pow(2 * M_PI, 0.5))) * exp(-0.5 * x * x);
+}
+
+
+// Loi standard cumulée
+double norm_cdf(const double x) {
+    double k = 1.0 / (1.0 + 0.2316419 * x);
+    double k_sum = k * (0.319381530 + k * (-0.356563782 + k * (1.781477937 + k * (-1.821255978 + 1.330274429 * k))));
+
+    if (x >= 0.0) {
+        return (1.0 - (1.0 / (pow(2 * M_PI, 0.5))) * exp(-0.5 * x * x) * k_sum);
+    }
+    else {
+        return 1.0 - norm_cdf(-x);
+    }
+}
+
+
+// Calcul des d1 et d2
+double d_j(const int j, const double S, const double K, const double r, const double v, const double T) {
+    return (log(S / K) + (r + (pow(-1, j - 1)) * 0.5 * v * v) * T) / (v * (pow(T, 0.5)));
+}
+
+// Prime Call B&S
+double call_price(const double S, const double K, const double r, const double v, const double T) {
+    return S * norm_cdf(d_j(1, S, K, r, v, T)) - K * exp(-r * T) * norm_cdf(d_j(2, S, K, r, v, T));
+}
+
+
+//Delta d'un Call
+double call_delta(const double S, const double K, const double r, const double v, const double T) {
+    return norm_cdf(d_j(1, S, K, r, v, T));
+}
+
+//Gamma Call
+double call_gamma(const double S, const double K, const double r, const double v, const double T) {
+    return norm_pdf(d_j(1, S, K, r, v, T)) / (S * v * sqrt(T));
+}
+
+// Vega Call
+double call_vega(const double S, const double K, const double r, const double v, const double T) {
+    return S * norm_pdf(d_j(1, S, K, r, v, T)) * sqrt(T);
+}
+
+//Theta Call
+double call_theta(const double S, const double K, const double r, const double v, const double T) {
+    return -(S * norm_pdf(d_j(1, S, K, r, v, T)) * v) / (2 * sqrt(T))
+        - r * K * exp(-r * T) * norm_cdf(d_j(2, S, K, r, v, T));
+}
+
+//Rho Call
+double call_rho(const double S, const double K, const double r, const double v, const double T) {
+    return K * T * exp(-r * T) * norm_cdf(d_j(2, S, K, r, v, T));
+}
+
+//Prix Put
+double put_price(const double S, const double K, const double r, const double v, const double T) {
+    return -S * norm_cdf(-d_j(1, S, K, r, v, T)) + K * exp(-r * T) * norm_cdf(-d_j(2, S, K, r, v, T));
+}
+
+// Calculate the European vanilla put Delta
+double put_delta(const double S, const double K, const double r, const double v, const double T) {
+    return norm_cdf(d_j(1, S, K, r, v, T)) - 1;
+}
+
+// Calculate the European vanilla put Gamma
+double put_gamma(const double S, const double K, const double r, const double v, const double T) {
+    return call_gamma(S, K, r, v, T); // Identical to call by put-call parity
+}
+
+// Calculate the European vanilla put Vega
+double put_vega(const double S, const double K, const double r, const double v, const double T) {
+    return call_vega(S, K, r, v, T); // Identical to call by put-call parity
+}
+
+// Calculate the European vanilla put Theta
+double put_theta(const double S, const double K, const double r, const double v, const double T) {
+    return -(S * norm_pdf(d_j(1, S, K, r, v, T)) * v) / (2 * sqrt(T))
+        + r * K * exp(-r * T) * norm_cdf(-d_j(2, S, K, r, v, T));
+}
+
+// Calculate the European vanilla put Rho
+double put_rho(const double S, const double K, const double r, const double v, const double T) {
+    return -T * K * exp(-r * T) * norm_cdf(-d_j(2, S, K, r, v, T));
+}
+
+
+//Pour les option asiatiques ////////////////////////////////////////////////
+//Methode par réduction de variance via variable de controle////////////////
+//Model de Kemma et Vorst /////////////////////////////////////////////////
+double LeftTerme(double S, double mu, double vol, double T, double K) {
+    return ((T / vol) * (log(S / K) - (0.5 * T) * (mu - 0.5 * pow(vol, 2))));
+}
+
+double Ez(double S, double mu, double vol, double T, double K,double w) {
+    double StrikePart = -K*(1-norm_cdf(w/(sqrt(pow(T,3)/3))));
+    double AssetPart = S*exp((mu-0.5 * pow(vol, 2))*(0.5*T)+pow(vol,2)*(T/6))*(1-norm_cdf((w-vol*pow(T,2)/3)/sqrt(pow(T,3)/3)));
+    return AssetPart + StrikePart;
+}
+
+
+std::vector<double> Dailyreturn(Matrice YahooData) {
+    std::vector<double> DoubleCloture;
+    std::vector<double> Cloture;
+    DoubleCloture.push_back(YahooData.valeurs[0][4]);
+    for (int i = 1; i < YahooData.n; i++) {
+        DoubleCloture.push_back(YahooData.valeurs[i][4]);
+        Cloture.push_back(DoubleCloture[i] / DoubleCloture[i - 1] - 1);
+    }
+    return Cloture;
+}
+
+double Moyenne(std::vector<double> Return) {
+    double somme = 0.0;
+    for (int i = 0; i < Return.size(); i++) {
+        somme = somme + Return[i];
+    }
+    return (somme / Return.size());
+}
+
+double EcartType(std::vector<double> Return) {
+    double somme = 0.0;
+    double Xbarre = Moyenne(Return);
+    for (int i = 0; i < Return.size(); i++) {
+        somme = somme + pow(Return[i], 2) - pow(Xbarre, 2);
+    }
+    return sqrt(somme / Return.size());
+}
+
+
+
+std::string GetDateAsof(bool DD) {
+    std::time_t now = std::time(0);
+    tm* ltm = localtime(&now);
+    std::ostringstream sa;
+    std::ostringstream sm;
+    std::ostringstream sj;
+    if (DD)
+    {
+        sa << (1900 + ltm->tm_year - 1);
+    }
+    else 
+        sa << (1900 + ltm->tm_year);
+
+    std::string Annee = sa.str();
+    std::string Mois;
+    std::string Jour;
+    if (ltm->tm_mon < 10)
+    {
+        sm << (1 + ltm->tm_mon);
+        Mois = "0" + sm.str();
+    }
+    else
+    {
+        sm << (1 + ltm->tm_mon);
+        Mois = sm.str();
+    }
+    if (ltm->tm_mday < 10)
+    {
+        sj<< (ltm->tm_mday);
+        Jour = "0" + sj.str();
+    }
+    else
+    {
+        sj << (ltm->tm_mday);
+        Jour = sj.str();
+    }
+    
+    return (Annee +"-"+ Mois +"-"+ Jour);
+
+}
+
+#endif /* utilitaire_h */
+
+
+
+
+#pragma once
